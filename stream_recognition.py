ef07539c22ea3da3b4d5a9c774b9600b2f2e9b37
takado <kornelgut@gmail.com>
2023-12-26 23:38:59 +0100

    async recognition

--- stream_recognition.py
+++ stream_recognition.py
@@ -1,125 +1,145 @@
+import multiprocessing
+import threading
+import queue
 import librosa.display
 import numpy as np
 import pyaudio
 from data_processing.mfcc import crop_or_pad
 from keras.models import load_model
-import datetime
-
-model = load_model('neural_network/eryk2.h5')
 
 length_seconds = 0.5
 target_sample_rate = 44100
 batch_size = 5
+frames_per_buffer = 1024
 
 
-def stream_recognition():
-    desired_length_in_samples = int(length_seconds * target_sample_rate)
-    snapshot_count = 5
-    shift_ms = 200  # adjust this to the amount of shift you want per snapshot
-    shift_samples = int(shift_ms / 1000 * target_sample_rate)
+class StreamRecognition:
+    def __init__(self):
+        print('loading model...')
+        self.model = load_model('neural_network/eryk2.h5')
 
-    p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16,
-        channels=1,
-        rate=target_sample_rate,
-        input=True,
-        frames_per_buffer=1024)
-    try:
-        buffer = np.zeros(desired_length_in_samples * 2, dtype=np.float32)  # double buffer length
-        while True:
-            mfccs_batch = []  # To store MFCCs for each snapshot
+    def recognize(self, frames):
+        desired_length_in_samples = int(length_seconds * target_sample_rate)
+        frame_size = int(len(frames) / 2)
+        frame_shift = int(len(frames) / batch_size)
+        mfccs_batch = []
+        for i in range(batch_size):
+            # Convert frames to numpy array and normalize to floating-point
+            audio_data = np.frombuffer(b''.join(frames[i * frame_shift:i * frame_shift + frame_size]),
+                dtype=np.int16).astype(np.float32) / 32768.0
+            audio_data = crop_or_pad(audio_data, desired_length_in_samples)
+            mfccs = librosa.feature.mfcc(y=audio_data, sr=target_sample_rate, n_mfcc=13)
+            mfccs_batch.append(mfccs)
 
-            # Read a new chunk into the second half of the buffer
-            for i in range(0, int(target_sample_rate / 1024 * (shift_ms / 1000))):
-                data = stream.read(1024)
-                new_data = np.frombuffer(data, dtype=np.int16).astype(np.float32) / 32768.0
-                buffer[desired_length_in_samples:desired_length_in_samples + len(new_data)] += new_data
+        # Convert list of mfccs to a numpy array for batch prediction
+        x_batch = np.stack(mfccs_batch, axis=0)  # Shape: (snapshot_count, time_steps, num_mfcc)
+        x_batch = np.expand_dims(x_batch, axis=-1)  # Add a channel dimension
 
-            # Create the snapshots and prepare the data for model prediction
-            for i in range(snapshot_count):
-                start = shift_samples * i
-                end = start + desired_length_in_samples
-                audio_data = buffer[start:end]
-                audio_data = crop_or_pad(audio_data, desired_length_in_samples)
+        # Perform batch prediction
+        results = self.model.predict(x_batch, verbose=0, batch_size=len(x_batch))
 
-                mfccs = librosa.feature.mfcc(y=audio_data, sr=target_sample_rate, n_mfcc=13)
-                mfccs_batch.append(mfccs)
-
-            # Convert list of mfccs to a numpy array for batch prediction
-            x_batch = np.stack(mfccs_batch, axis=0)  # Shape: (snapshot_count, time_steps, num_mfcc)
-            x_batch = np.expand_dims(x_batch, axis=-1)  # Add a channel dimension
-
-            # Perform batch prediction
-            results = model.predict(x_batch, verbose=1)
-
-            for i, result in enumerate(results):
-                prediction = np.argmax(result)
-                percent = round(result[prediction] * 100, 2)
-                if prediction == 0 and percent < 95:
-                    prediction = 1
+        for i, result in enumerate(results):
+            prediction = np.argmax(result)
+            percent = round(result[prediction] * 100, 2)
+            if prediction == 0 and percent < 95:
+                prediction = 1
+            if prediction == 0:
                 label = 'Keyword! <<<<<' if prediction == 0 else 'none'
                 print(f'Snapshot {i}: {label} {percent}%')
 
-            # Roll the buffer before reading the next chunk
-            buffer[:desired_length_in_samples] = buffer[desired_length_in_samples:]
-            buffer[desired_length_in_samples:] = 0
+    def stream_recognition(self):
+        p = pyaudio.PyAudio()
+        stream = p.open(format=pyaudio.paInt16,
+                        channels=1,
+                        rate=target_sample_rate,
+                        input=True,
+                        frames_per_buffer=1024)
+        try:
+            p = None
+            while True:
+                # Collect audio data
+                frames = []
+                print("Recording...")
+                for i in range(0, int(target_sample_rate / 1024 * length_seconds * 2)):
+                    data = stream.read(1024)
+                    frames.append(data)
+                print("Finished recording.")
+                self.recognize(frames)
 
-    finally:
-        # Close the stream
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
+        finally:
+            # Close the stream
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
 
+    def start_stream(self, audio_queue):
+        # Open the stream in a different thread to prevent blocking
+        p = pyaudio.PyAudio()
+        stream = p.open(format=pyaudio.paInt16,
+            channels=1,
+            rate=target_sample_rate,
+            input=True,
+            frames_per_buffer=frames_per_buffer)
+        try:
+            while True:
+                # Read chunks from the audio stream and put them in the queue
+                for i in range(0, int(target_sample_rate / frames_per_buffer * length_seconds * 2)):
+                    data = stream.read(frames_per_buffer)
+                    audio_queue.put(data)
+        finally:
+            # Close the stream
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
 
-def recognize(frames):
-    desired_length_in_samples = int(length_seconds * target_sample_rate)
-    frame_size = int(len(frames) / 2)
-    frame_shift = int(len(frames) / batch_size)
-    # mfccs_batch = []
-    for i in range(batch_size):
-        # Convert frames to numpy array and normalize to floating-point
-        audio_data = np.frombuffer(b''.join(frames[i * frame_shift:i * frame_shift + frame_size]),
-            dtype=np.int16).astype(np.float32) / 32768.0
-        audio_data = crop_or_pad(audio_data, desired_length_in_samples)
-        mfccs = librosa.feature.mfcc(y=audio_data, sr=target_sample_rate, n_mfcc=13)
-        # mfccs_batch.append(mfccs)
+    def stream_recognition_full_async(self):
+        audio_queue = queue.Queue()
 
-        x = np.expand_dims(mfccs, axis=0)
-        x = np.expand_dims(x, axis=-1)
+        # start the audio stream in a separate thread
+        audio_thread = threading.Thread(target=self.start_stream, args=(audio_queue,))
+        audio_thread.start()
 
-        result = model.predict(x, verbose=0)[0]
-        prediction = np.argmax(result)
-        percent = round(result[prediction] * 100, 2)
-        if prediction == 0 and percent < 95:
-            prediction = 1
-        label = 'Keyword! <<<<<' if prediction == 0 else 'none'
-        print(f'{label} {percent}%')
+        try:
+            while True:
+                # Collect audio data
+                frames = []
+                while len(frames) < int(target_sample_rate / frames_per_buffer * length_seconds * 2):
+                    frames.append(audio_queue.get())  # Take data from the queue
 
+                # Process the audio in a separate thread to make it non-blocking
+                threading.Thread(target=self.recognize, args=(frames,)).start()
+        except KeyboardInterrupt:
+            # User interrupted the process, stop the audio thread
+            audio_thread.join()
 
-def stream_recognition2():
-    p = pyaudio.PyAudio()
-    stream = p.open(format=pyaudio.paInt16,
-                    channels=1,
-                    rate=target_sample_rate,
-                    input=True,
-                    frames_per_buffer=1024)
-    try:
-        while True:
-            # Collect audio data
-            frames = []
-            print("Recording...")
-            for i in range(0, int(target_sample_rate / 1024 * length_seconds * 2)):
-                data = stream.read(1024)
-                frames.append(data)
-            print("Finished recording.")
-            recognize(frames)
+    def stream_recognition_async(self):
+        p = pyaudio.PyAudio()
+        stream = p.open(format=pyaudio.paInt16,
+            channels=1,
+            rate=target_sample_rate,
+            input=True,
+            frames_per_buffer=frames_per_buffer)
 
-    finally:
-        # Close the stream
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
+        try:
+            while True:
+                print("Recording...")
+                frames = []
+                for i in range(0, int(target_sample_rate / frames_per_buffer * length_seconds * 2)):
+                    data = stream.read(frames_per_buffer)
+                    frames.append(data)
+                print("Finished recording.")
+
+                # Offload processing to another thread
+                threading.Thread(target=self.recognize, args=(frames,)).start()
+
+        except KeyboardInterrupt:
+            print("Stopping...")
+        finally:
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
 
 
 if __name__ == '__main__':
-    stream_recognition2()
+    sr = StreamRecognition()
+    sr.stream_recognition_async()
